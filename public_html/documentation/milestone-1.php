b6c36c85d83893b2dd21538e88064682dfe9ce33
Matt Harris <mateodelay@gmail.com>
2015-10-25 16:08:23 -0600

    Fixed comment tags

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -1,11 +1,11 @@
 <!DOCTYPE HTML>
 <html lang="en">
-	<-Page header elements for Milestone One page. Sets meta charset and title->
+	<!-Page header elements for Milestone One page. Sets meta charset and title->
 	<head>
 		<meta charset="utf-8" />
 		<title>Personal Website Project Milestone One</title>
 	</head>
-	<-Body for Milestone One page->
+	<!-Body for Milestone One page->
 	<body>
 		<h1>Outline of "Author Page" Personal Website Project</h1>
 

2a46d6ef4c89d5942f9b474c299542cd15c67834
Matt Harris <mateodelay@gmail.com>
2015-10-25 17:03:26 -0600

    Edited  outline paragraph

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -13,13 +13,13 @@
 		<p>For my personal website project I will create a website for an e-book author to showcase their works. This site
 			will help my wife promote the novel she is currently preparing for self-publication in e-book format, and will
 			help me secure freelance work creating author pages for the many self-published authors currently using inferior
-			websites to promote their work.
+			 (or none at all) websites to promote their work.
 		</p>
 		<!-Potential features of "Author Page" project>
 		<h2>Features of "Author Page"</h2>
 		<p>As an exercise in front-end development, this site will contain a main page featuring the author's name and
 		most current and popular works, along with individual pages for each work containing convenient
-		links to view the works in 3rd party e-commerce sites(amazon.com, barnesandnoble.com, ect..),
+		links to view the works in 3rd party e-commerce sites (amazon.com, barnesandnoble.com, ect..),
 		provide a mailing list sign-up for fans, a blog, and pages for any other information an
 		author might wish to convey to the public.
 		</p>

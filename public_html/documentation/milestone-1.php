3ecfaa92eba09c9cadc0841bac380082bfd4c149
Matt Harris <mateodelay@gmail.com>
2015-10-25 16:03:46 -0600

    Added head tag and body tag.

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -1,2 +1,14 @@
 <!DOCTYPE HTML>
-	<meta charset="utf-8" />
+<html lang="en">
+	<-Page header elements for Milestone One page. Sets meta charset and title->
+	<head>
+		<meta charset="utf-8" />
+		<title>Personal Website Project Milestone One</title>
+	</head>
+	<-Body for Milestone One page->
+	<body>
+		<h1></h1>
+
+	</body>
+
+</html>

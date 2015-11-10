21362dd5baf8823fbf2830d8963d3968e015d62d
Matt Harris <mateodelay@gmail.com>
2015-11-09 22:23:42 -0700

    Created document and began profile of Andrea Baldwin.

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,57 @@
+<!DOCTYPE HTML>
+<html lang="eng">
+	<!-Page header elements for Milestone Two page. Sets meta charset and title->
+	<head>
+		<meta charset="utf-8">
+		<title>PWP Milestone 2</title>
+	</head>
+	<!-Body of Milestone 2 page.->
+	<body>
+		<!- Heading one "Andrea Baldwin"->
+		<h1>Andrea Baldwin</h1>
+		<!-Paragraph explaining Andrea Baldwin's persona and use case scenarios->
+		<p>Andrea Baldwin is a 42 year old married secretary. After 3 children and 20 years of marriage,
+			Andrea's life has become rather routine; work, pick the kids up from soccer practice, have dinner with the family,
+			do the laundry, and finally, watch tv with her husband. Andrea never thinks about running away from from her boring life
+			in Kentucky thanks to her Kindle Paperwhite, and the numerous forbidden romances and steamy adventures of the
+			Kindle Store.
+			instead preferring to get comfortable in the shallow end before wading deeper into life experiences.
+			When a new novelty grabs the reigns of Shawn's attention it doesn't let go until Shawn has tried the
+			new activity. Whether it's gardening or skiing, Shawn won't lift a single shovel or pole until he
+			completes several hours of internet research from his two primary devices:
+		</p>
+		<!-List of Shawn's Devices>
+		<ol>
+			<!-iPhone 5s Primary device for researching new interests->
+			<li>iPhone 5s 16 GB</li>
+			<ul>
+				<li>Screen size: 4 inch screen</li>
+				<li>Screen resolution: 1136 x 640</li>
+				<li>Storage capacity: 16 GB</li>
+				<li>GSM Radio: 4G LTE</li>
+				<li>Carrier: AT&T</li>
+			</ul>
+			<br>
+			<p>Shawn is rarely found without his trusty iPhone 5s. The 4 inch screen is the perfect balance between
+				viewability and functionality and Shawn does everything from it. Siri and Shawn are on a first name
+				basis with Siri often giving Shawn recommendations for restaurants, movies and other activities.
+				When Shawn needs to know something right away his iPhone has the reliability he requires to always
+				have information at glance, without unexpected hiccups keeping him from the knowledge he craves.</p>
+			<!-Macbook Air Secondary Device used for web surfing, social media, banking, travel, shopping as well
+			as document creation and book keeping->
+			<li>Macbook Air 13.3"</li>
+			<ul>
+				<li>Intel Core i5 1.6GHz</li>
+				<li>13.3" 1440 &times; 900 LED Backlit Display</li>
+				<li>128GB SSD Flash Storage</li>
+				<li>Mac OS X Yosemite</li>
+			</ul>
+			<br>
+			<p>When his iPhone just isn't powerful enough for the task, Shawn opens up his Macbook and performs
+				serious work on the 13'3 inch HD display. The core i5 1.6ghz processor is plenty powerful for the
+				times Shawn needs to edit photos, update his financial spreadsheets, or create a sample portfolio for a
+				potential client. Shawn also enjoys surfing the web and learning about new hobbies from the beautiful
+				1440 &times; 900 LED back-lit display.
+			</p>
+	</body>
+</html>

1927f1448f24dd360da4ef46b0579ac769c4ac75
Matt Harris <mateodelay@gmail.com>
2015-12-13 21:25:10 -0700

    Footer exists with basic content..

--- public_html/documentation/php/template/footer.php
+++ public_html/documentation/php/template/footer.php
@@ -1,6 +1,6 @@
 <!-- Footer for Juliana Connors Author Page -->
 <footer class="footer">
 	<div class="container">
-		<p class="text-muted">Place sticky footer content here.</p>
+		<p class="text-muted">Created by www.mattharr.com</p>
 	</div>
 </footer>

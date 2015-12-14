662604f8f8ee7625bbbf333337837a44ee18b746
Matt Harris <mateodelay@gmail.com>
2015-12-13 21:47:01 -0700

    Some type of header.

--- public_html/documentation/php/template/footer.php
+++ public_html/documentation/php/template/footer.php
@@ -1,6 +1,6 @@
 <!-- Footer for Juliana Connors Author Page -->
 <footer class="footer">
 	<div class="container">
-		<p class="text-muted">Created by www.mattharr.com</p>
+		<p >Created by www.mattharr.com</p>
 	</div>
 </footer>

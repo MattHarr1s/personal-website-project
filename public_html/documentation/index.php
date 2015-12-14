184ea89c1c31b5070ba33e4548233c16ebf0b5ba
Matt Harris <mateodelay@gmail.com>
2015-12-13 19:06:53 -0700

    Building file structure.

---
+++ public_html/documentation/index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE HTML>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-COMPATIBLE" content="ie=edge">
+	<meta name ="viewport" content="width=device-width, initial-scale=1">
+	<title>Juliana Connors</title>
+	<!--Bootstrap-->
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"
+			integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ=="
+			crossorigin="anonymous">
+
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css"
+			integrity="sha384-aUGj/X2zp5rLCbBxumKTCw2Z50WgIr1vs/PFN4praOTvYXWlVyh2UtNUU0KAUhAX"
+			crossorigin="anonymous">
+
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"
+			  integrity="sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ=="
+			  crossorigin="anonymous">
+	</script>
+
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<-- HTML5 shiv and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+	<script src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+
+</head>
+<body>
+	<nav class="navbar navbar-inverse">
+		<!-- logo and mobile toggle button get grouped together for better mobile display -->
+		<div class="navbar-header">
+			<!-- this is the mobile menu button -->
+			<button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target="#main-menu">
+				<span class="sr-only">main menu</span>
+				<span class="glyphicon glyphicon-menu-hamburger"></span>
+			</button>
+			<a class="navbar-brand" href="#"></a>
+		</div>
+
+		<!-- here are your main nav links, grouped for toggling -->
+		<div class="collapse navbar-collapse" id="main-menu">
+			<ul class="nav navbar-nav navbar-right">
+				<li><a href="#">Home</a></li>
+				<li><a href="#">Link1</a></li>
+				<li><a href="#">Link2</a></li>
+				<li><a href="#">Link3</a></li>
+			</ul>
+		</div>
+	</nav>
+
+	<!--<div class="container"-->
+			<div class="row">
+				<div class=".col-md-4">
+					todo-matt add image
+				</div>
+				<div class=".col-md-offset-4">
+					todo-matt add links
+				</div>
+			</div>
+		</div>
+</body>
+</div>
+			<!--button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="navbar"
+					  aria-expanded="false" aria-controls="navbar">
+				<span class="sr-only">Toggle navigation</span>

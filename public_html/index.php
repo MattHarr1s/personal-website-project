73384907217e92f0165d2e9768e60df0509010dd
Matt Harris <mateodelay@gmail.com>
2015-12-17 16:41:17 -0700

    Still working on home page.

--- public_html/index.php
+++ public_html/index.php
@@ -1,6 +1,6 @@
 <?php
 $CURRENT_DIR = __DIR__;
-$PAGE_TITLE = "Juliana Connors";
+$PAGE_TITLE = "Juliana Conners";
 require_once("php/template/utilities.php");
 ?>
 <body class="sfooter">
@@ -11,29 +11,28 @@ require_once("php/template/utilities.php");
 		<main>
 			<div class="container-fluid">
 				<div class="row">
-
 					<div class="col-sm-4 col-sm-offset-1">
+						<h2>New Release: Jensen</h2>
 						<div class="thumbnail">
 							<a href="http://www.amazon.com/Jensen-Military-Romance-Bradford-Brothers-ebook/dp/B018MKYDHA/ref=sr_1_sc_1?s=digital-text&ie=UTF8&qid=1450371256&sr=8-1-spell&keywords=julianna+conners" target="_blank">
-								<img src="img/jensen-cover-500.jpg" class="img-responsive" alt="jensen cover">
-								<div class="caption">
-									text here
-								</div>
+							<img src="img/jensen-cover-500.jpg" class="img-responsive" alt="jensen cover">
 							</a>
+							<div class="caption">
+							<P>She’s so stuck up. She obviously has something to prove. Her personality is so not my type. But all I want to do is put my hands all over that curvy ass of hers.
+							</P>
+							</div>
 						</div>
 					</div>
 					<div class="col-sm-4 col-sm-offset-1">
+						<h2>Coming Soon: Harlow</h2>
 						<div class="thumbnail">
-							<a href="http://www.amazon.com/Jensen-Military-Romance-Bradford-Brothers-ebook/dp/B018MKYDHA/ref=sr_1_sc_1?s=digital-text&ie=UTF8&qid=1450371256&sr=8-1-spell&keywords=julianna+conners" target="_blank">
-								<img src="img/jensen-cover-500.jpg" class="img-responsive" alt="jensen cover">
-								<div class="caption">
-									text here
-								</div>
-							</a>
+							<img src="img/harlow-cover-500.jpg" class="img-responsive" alt="jensen cover">
+							<div class="caption">
+								<p>Book 2 in the Bradford Brothers Series</p>
+							</div>
 						</div>
 					</div>
-
-				</div><!-- .row -->
+				</div><!-- .row -->o
 			</div><!-- .container-fluid -->
 		</main>
 	</div><!-- .sfooter-content -->
@@ -41,4 +40,4 @@ require_once("php/template/utilities.php");
 		<?php require_once("php/template/footer.php"); ?>
 	</footer>
 </body>
-</html>
+

cb84fbc61b110100c6501f9759e1c3d49bfd3475
Matt Harris <mateodelay@gmail.com>
2015-12-18 09:30:49 -0700

    Changed container for pictures.

--- public_html/index.php
+++ public_html/index.php
@@ -12,27 +12,33 @@ require_once("php/template/utilities.php");
 			<div class="container-fluid">
 				<div class="row">
 					<div class="col-sm-4 col-sm-offset-1">
-						<h2>New Release: Jensen</h2>
-						<div class="thumbnail">
-							<a href="http://www.amazon.com/Jensen-Military-Romance-Bradford-Brothers-ebook/dp/B018MKYDHA/ref=sr_1_sc_1?s=digital-text&ie=UTF8&qid=1450371256&sr=8-1-spell&keywords=julianna+conners" target="_blank">
-							<img src="img/jensen-cover-500.jpg" class="img-responsive" alt="jensen cover">
+						<h2 class="text-center">New Release: Jensen</h2>
+
+						<div class="text-center">
+							<a href="http://www.amazon.com/Jensen-Military-Romance-Bradford-Brothers-ebook/dp/B018MKYDHA/ref=sr_1_sc_1?s=digital-text&ie=UTF8&qid=1450371256&sr=8-1-spell&keywords=julianna+conners"
+								target="_blank">
+								<img src="img/jensen-cover-500.jpg" class="img-responsive center-block" alt="jensen cover">
 							</a>
+
 							<div class="caption">
-							<P>She’s so stuck up. She obviously has something to prove. Her personality is so not my type. But all I want to do is put my hands all over that curvy ass of hers.
-							</P>
+								<P>She’s so stuck up. She obviously has something to prove. Her personality is so not my type.
+									But all I want to do is put my hands all over that curvy ass of hers.
+								</P>
 							</div>
 						</div>
 					</div>
 					<div class="col-sm-4 col-sm-offset-1">
-						<h2>Coming Soon: Harlow</h2>
-						<div class="thumbnail">
-							<img src="img/harlow-cover-500.jpg" class="img-responsive" alt="jensen cover">
+						<h2 class="text-center ">Coming Soon: Harlow</h2>
+
+						<div class="text-center">
+							<img src="img/harlow-cover-500.jpg" class="img-responsive center-block" alt="jensen cover">
+
 							<div class="caption">
 								<p>Book 2 in the Bradford Brothers Series</p>
 							</div>
 						</div>
 					</div>
-				</div><!-- .row -->o
+				</div><!-- .row -->
 			</div><!-- .container-fluid -->
 		</main>
 	</div><!-- .sfooter-content -->
--- public_html/php/template/header.php
+++ public_html/php/template/header.php
@@ -1,23 +1,24 @@
-<nav class="navbar navbar-default">
+<nav class="navbar navbar-inverse">
 	<div class="container-fluid">
 		<!-- Brand and toggle get grouped for better mobile display -->
 		<div class="navbar-header">
-			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+					  data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
 				<span class="sr-only">Toggle navigation</span>
 				<span class="icon-bar"></span>
 				<span class="icon-bar"></span>
 				<span class="icon-bar"></span>
 			</button>
-			<a class="navbar-brand" href="<?php echo $PREFIX; ?>">Juliana Conners</a>
+			<a class="navbar-brand white-text" href="<?php echo $PREFIX; ?>">Juliana Conners</a>
 		</div>
 
 		<!-- Collect the nav links, forms, and other content for toggling -->
 		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-			<ul class="nav navbar-nav">
-				<li class="active"><a href="<?php echo $PREFIX; ?>">Home</a></li>
-				<li><a href="<?php echo $PREFIX; ?>catalog/index.php">Catalog</a></li>
-				<li><a href="<?php echo $PREFIX; ?>mailing-list/index.php">Mailing List</a></li>
-				<li><a href="<?php echo $PREFIX; ?>about-me/index.php">About Me</a></li>
+			<ul class="nav navbar-nav navbar-right">
+				<li><a class="white-text text-right" href="<?php echo $PREFIX; ?>">Home</a></li>
+				<li><a class="white-text text-right" href="<?php echo $PREFIX; ?>catalog/index.php">Catalog</a></li>
+				<li><a class="white-text text-right" href="<?php echo $PREFIX; ?>mailing-list/index.php">Mailing List</a></li>
+				<li><a class="white-text text-right" href="<?php echo $PREFIX; ?>about-me/index.php">About Me</a></li>
 			</ul>
 		</div><!-- /.navbar-collapse -->
 	</div><!-- /.container-fluid -->
